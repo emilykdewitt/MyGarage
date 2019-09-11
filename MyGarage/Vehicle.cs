f157de537cc29d7b434b33fce5a4938c2273379e
Emily DeWitt <47046126+emilykdewitt@users.noreply.github.com>
2019-09-10 18:40:07 -0700

    Add project files.

---
+++ MyGarage/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyGarage
+{
+    class Vehicle
+    {
+        public int GasCapacity { get; set; }
+
+        public string Color { get; set; }
+
+        public int PassengerOccupancy { get; set; }
+
+        public void Refuel()
+        {
+            Console.WriteLine($"You now have a full tank of {GasCapacity} gallons");
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("Vroom vroom!");
+        }
+
+        public void Brake()
+        {
+            Console.WriteLine("Your speed is now 0 miles per hour");
+        }
+    }
+}
